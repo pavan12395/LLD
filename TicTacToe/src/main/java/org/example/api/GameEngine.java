510bc3ff18d443ce9799f21c77324113b477decb
Pavan Kongara <pavan.kongara@nurture.farm>
2024-01-04 13:10:57 +0530

    Added changes

--- TicTacToe/src/main/java/org/example/api/GameEngine.java
+++ TicTacToe/src/main/java/org/example/api/GameEngine.java
@@ -1,16 +1,34 @@
 package org.example.api;
 
 
+import javafx.css.Rule;
 import org.example.game.Board;
 import org.example.game.Cell;
 import org.example.game.GameState;
 import org.example.game.Move;
 import org.example.boards.TicTacToeBoard;
+import org.example.rulengines.RuleEngine;
+import org.example.rulengines.TicTacToeRuleEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameEngine {
+
+    private Map<String,RuleEngine> ruleEngineMap;
+
+    public GameEngine(){
+        ruleEngineMap = new HashMap<>();
+        ruleEngineMap.put(TicTacToeBoard.class.getName(),new TicTacToeRuleEngine());
+    }
     public GameState move(Board board, Move move) throws Exception {
-        board.move(move);
-        return board.getState();
+        RuleEngine ruleEngine = ruleEngineMap.get(board.getClass().getName());
+        if(ruleEngine.checkMove(move,board)){
+            board.setCell(move.getCell(),move.getEntity().getPlayerSymbol());
+            return ruleEngine.getState(board);
+        } else {
+            throw new Exception("Invalid Move!");
+        }
     }
 
     public Board start(Cell.BoardType boardType) throws Exception {
